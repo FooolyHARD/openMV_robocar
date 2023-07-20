ab595b24e74a90154d47eb038e7dab51beb70eb9
Senechka <Senechka@tor.onion>
2023-07-20 14:33:58 +0300

    initial commit 2

--- drive.py
+++ drive.py
@@ -1,25 +1,6 @@
-# Untitled - By: shizlong - чт июл. 20 2023
-
-import pyb, time
-centerAngle = 7
-leftAngle = -14
-rightAngle = 24
-
-
-servo1 = pyb.Servo(1)
-servo1.angle(7)
-time.sleep(1)
-
 tim = pyb.Timer(2, freq = 50)
 motorPin1 = tim.channel(3, pyb.Timer.PWM, pin=pyb.Pin('P4'))
 motorPin2 = tim.channel(4, pyb.Timer.PWM, pin=pyb.Pin('P5'))
-
-motorPin1.pulse_width_percent(0)
-motorPin2.pulse_width_percent(10)
-time.sleep(1)
-motorPin1.pulse_width_percent(100)
-motorPin2.pulse_width_percent(100)
-time.sleep(1)
 
 def drive(x):
     if x > 0:
---
+++ servo.py
@@ -0,0 +1,9 @@
+import pyb, time
+centerAngle = 7
+leftAngle = -14
+rightAngle = 24
+
+
+servo1 = pyb.Servo(1)
+servo1.angle(7)
+time.sleep(1)
